cc2eccdf587686c970c1917f53866d299361664d
Zac Willmington <zac.willmington@gmail.com>
2018-07-17 00:59:17 +0000

    automatically backed up by learn

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,4 +1,10 @@
 require "pry"
 class StoreAdminController < ApplicationController
-    
+    def home
+        render :layout => "admin"
+    end
+
+    def orders
+        binding.pry
+    end
 end
